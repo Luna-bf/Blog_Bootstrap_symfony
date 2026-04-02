4d6067b6f9f0acfa2e66a95e55ae2c71ea64e186
Luna-bf <lburattifayolle@gmail.com>
2026-04-02 09:47:24 +0200

    Ajout d'attributs et autres paramètres pour le formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,9 +19,49 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('username', TextType::class, [
+                // Label personnalisé
+                'label' => 'Nom d\'utilisateur',
+
+                // Attributs de la div générée par $builder
+                'row_attr' => [
+                    'class' => 'mb-4'
+                ],
+
+                // Attributs du label
+                'label_attr' => [
+                    'class' => 'form-label text-light'
+                ],
+
+                // Attributs de l'input
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                // Label personnalisé
+                'label' => 'Adresse mail',
+
+                // Attributs de la div générée par $builder
+                'row_attr' => [
+                    'class' => 'mb-4'
+                ],
+
+                // Attributs du label
+                'label_attr' => [
+                    'class' => 'form-label text-light'
+                ],
+
+                // Attributs de l'input
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'mapped' => false,
+                'label_attr' => [
+                    'class' => 'me-2 text-center text-light'
+                ],
                 'constraints' => [
                     new IsTrue(
                         message: 'You should agree to our terms.',
@@ -31,13 +73,31 @@ class RegistrationFormType extends AbstractType
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+
+                // Label personnalisé
+                'label' => 'Mot de passe',
+
+                // Attributs de la div générée par $builder
+                'row_attr' => [
+                    'class' => 'mb-4'
+                ],
+
+                // Attributs du label
+                'label_attr' => [
+                    'class' => 'form-label text-light'
+                ],
+
+                // Attributs de l'input
+                'attr' => [
+                    'class' => 'form-control',
+                ],
                 'constraints' => [
                     new NotBlank(
-                        message: 'Please enter a password',
+                        message: 'Veuillez saisir un mot de passe.',
                     ),
                     new Length(
                         min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        minMessage: 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
