45bb85d5e23b321903edf215254ada2f23cfa291
Luna-bf <lburattifayolle@gmail.com>
2026-04-03 07:43:56 +0200

    Ajout de routes pour les contrôleurs

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/', name: 'post_')]
 final class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(PostRepository $repo): Response
     {
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $repo->findAll(),
         ]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/user', name: 'user_')]
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/user', name: 'index')]
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => 'Hello World !',
         ]);
     }
 }
