76e39d5a354eb5ef4ad1bb9e28001ef88e7a7522
Luna-bf <lburattifayolle@gmail.com>
2026-04-07 09:39:59 +0200

    Màj du traitement des données du formulaire

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Repository\CategoryRepository;
 use App\Repository\PostRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,9 +37,18 @@ final class PostController extends AbstractController
         $createPostForm->handleRequest($request);
 
         // Si le formulaire est envoyé (isSubmitted) et que ces données sont valides (isValid())
-        if($createPostForm->isSubmitted() && $createPostForm->isValid()) {
+        if ($createPostForm->isSubmitted() && $createPostForm->isValid()) {
 
-            $em->persist($post); // Prépare la requête (ici, la création d'un nouveau post)
+            // Je récupère toutes les données du formulaire (rempli) et les injectes dans l'objet $post
+            $completePost = $createPostForm->getData();
+
+            // J'ajoute la date de création (setter) au champ createdAt du formulaire (caché)
+            $completePost->setCreatedAt(new \DateTimeImmutable());
+
+            $user = $this->getUser(); // Je récupère les données l'utilisateur connecté
+            $completePost->setMyUser($user); // Puis j'associe les données de l'utilisateur connecté à la publication (setter)
+
+            $em->persist($completePost); // Prépare la requête (ici, la création d'un nouveau post)
             $em->flush(); // Exécute la requête préparée
 
             return $this->redirectToRoute('user_index');
