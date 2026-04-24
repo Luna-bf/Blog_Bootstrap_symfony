ccc2ce1584f6280be6516d07fc7075e052bda8d8
Luna-bf <lburattifayolle@gmail.com>
2026-04-24 07:26:23 +0200

    Add

---
+++ src/Entity/PostController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[Route('/', name: 'post_')]
+final class PostController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(PostRepository $postRepo, CategoryRepository $categoryRepo): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepo->findAll(),
+            'categories' => $categoryRepo->findAll(),
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'show')]
+    public function showPost(Post $post) {
+        
+        return $this->render('post/showPost.html.twig', [
+            'post' => $post
+        ]);
+    }
+
+    #[Route('/post/forms/create', name: 'create')]
+    public function createNewPost(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/images')] string $imagesDirectory): Response
+    {
+        $message = ""; // Initialisation de la variable contenant le message d'erreur pour les fichiers (FileException)
+
+        // Nouvelle instance de la classe (entité) Post
+        $post = new Post;
+
+        // Initialisation du formulaire
+        $createPostForm = $this->createForm(PostType::class, $post);
+
+        // Traitement du formulaire
+        $createPostForm->handleRequest($request);
+
+        // Si le formulaire est envoyé (isSubmitted) et que ces données sont valides (isValid())
+        if ($createPostForm->isSubmitted() && $createPostForm->isValid()) {
+
+            // Récupère la valeur de l'input "image_name" et la stocke dans la variable $image
+            $image = $createPostForm->get('image_name')->getData();
+
+            // Je récupère toutes les données du formulaire (rempli) et les injectes dans l'objet $post
+            $completePost = $createPostForm->getData();
+
+            // Si la valeur du champ de saisie "image_name" (stockée dans la variable $image) n'est pas vide...
+            if ($image) {
+                // Je récupère le nom original de l'image
+                $originalImageName = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // Puis j'utilise un slug pour créer un nom sécurisé
+                $safeFilename = $slugger->slug($originalImageName);
+
+                /* Enfin, je crée le nom définitif de l'image en utilisant la valeur de la variable $safeFileName, j'inclus
+                un identifiant unique grâce à la fonction "uniqid()", puis je précise l'extension de l'image grâce à la fonction
+                "guessExtension()" appliquée sur la variable $image.
+                */
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $image->guessExtension();
+
+                try {
+                    // Envoie l'image dans le dossier adéquat
+                    $image->move($imagesDirectory, $newFilename);
+                } catch (FileException $e) {
+                    $message = $e;
+                }
+
+                // Stocke le nom de l'image dans la BDD
+                $post->setImageName($newFilename);
+            }
+
+            /*
+            La date (DateTimeImmutable) sera envoyée par le contrôleur grâce au setter "setCreatedAt". Effectuer l'envoi de cette
+            donnée côté serveur est plus sécurisé car l'utilisateur n'y aura jamais accès (envoyer cette donnée via un formulaire
+            lui donnerais la possibilité de modifier une donnée sensible, ce qui n'est pas désirable).
+            */
+            $completePost->setCreatedAt(new \DateTimeImmutable());
+
+            $user = $this->getUser(); // Je récupère les données l'utilisateur connecté
+            $completePost->setMyUser($user); // Puis j'associe les données de l'utilisateur connecté à la publication (setter)
+
+            $em->persist($completePost); // Prépare la requête (ici, la création d'un nouveau post)
+            $em->flush(); // Exécute la requête préparée
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('post/forms/createPost.html.twig', [
+            'createPostForm' => $createPostForm,
+            'message' => $message
+        ]);
+    }
+
+    // {id} est un paramètre dynamique : il va récupérer l'identifiant associé au post à modifier pour afficher le formulaire adéquat
+    #[Route('/post/forms/update/{id}', name: 'update')]
+    public function updatePost(Post $post, Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/images')] string $imagesDirectory): Response
+    {
+        $message = ""; // Initialisation de la variable contenant le message d'erreur pour les fichiers (FileException)
+
+        // Initialisation du formulaire
+        $updatePostForm = $this->createForm(PostType::class, $post); // $post représente une ligne récupérée dans la BDD
+
+        // Traitement du formulaire
+        $updatePostForm->handleRequest($request);
+
+        // Si le formulaire est envoyé (isSubmitted) et que ces données sont valides (isValid())
+        if ($updatePostForm->isSubmitted() && $updatePostForm->isValid()) {
+
+            // Récupère la valeur de l'input "image_name" et la stocke dans la variable $image
+            $image = $updatePostForm->get('image_name')->getData();
+
+            // Je récupère toutes les données du formulaire (rempli) et les injectes dans l'objet $post
+            $completePost = $updatePostForm->getData();
+
+            // Si la valeur du champ de saisie "image_name" (stockée dans la variable $image) n'est pas vide...
+            if ($image) {
+                // Je récupère le nom original de l'image
+                $originalImageName = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // Puis j'utilise un slug pour créer un nom sécurisé
+                $safeFilename = $slugger->slug($originalImageName);
+
+                /* Enfin, je crée le nom définitif de l'image en utilisant la valeur de la variable $safeFileName, j'inclus
+                un identifiant unique grâce à la fonction "uniqid()", puis je précise l'extension de l'image grâce à la fonction
+                "guessExtension()" appliquée sur la variable $image.
+                */
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $image->guessExtension();
+
+                try {
+                    // Envoie l'image dans le dossier adéquat
+                    $image->move($imagesDirectory, $newFilename);
+                } catch (FileException $error) {
+                    $message = $error;
+                }
+
+                // Stocke le nom de l'image dans la BDD
+                $post->setImageName($newFilename);
+            }
+
+            $em->persist($post);
+            $em->flush(); // Exécute la requête préparée
+
+            return $this->redirectToRoute('user_index');
+        }
+
+        return $this->render('post/forms/updatePost.html.twig', [
+            'updatePostForm' => $updatePostForm,
+            'message' => $message
+        ]);
+    }
+
+    // {id} est un paramètre dynamique : il va récupérer l'identifiant associé au post à modifier pour afficher le formulaire adéquat
+    #[Route('/post/{id}/delete', name: 'delete')]
+    public function deletePost(PostRepository $post, Request $request, EntityManagerInterface $em): Response
+    {
+        // Récupère la valeur de l'input nommé "token" (le jeton CSRF)
+        $submittedToken = $request->getPayload()->get('token');
+
+        // Vérifie si le jeton CSRF nommé "delete-post" correspond à la valeur récupérée par la variable $submittedToken
+        if ($this->isCsrfTokenValid('delete-post', $submittedToken)) {
+
+            $em->remove($post);
+            $em->flush();
+
+            $this->addFlash('success', 'La publication a été supprimée avec succès.');
+
+            return $this->redirectToRoute('user_index');
+        } else {
+            throw new Exception("Le jeton CSRF est invalide.");
+        }
+    }
+}
---
+++ src/Entity/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserSettingsType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+// #[IsGranted('ROLE_USER')] : Ces routes ne sont accessibles que si l'utilisateur est connecté
+#[IsGranted('ROLE_USER')]
+#[Route('/user', name: 'user_')]
+final class UserController extends AbstractController
+{
+    /*
+    La ligne "#[CurrentUser] User $user" va récupérer toutes les informations de l'utilisateur actuellement connecté :
+
+    - User $user : injecte l'objet (l'entité) User dans la variable $user.
+    - #[CurrentUser] : est un attribut PHP qui va me permettre de récupérer toutes les informations de l'utilisateur connecté
+    */
+    #[Route('', name: 'index')]
+    public function index(PostRepository $repo, #[CurrentUser] User $user): Response
+    {
+        $user_id = $user->getId(); // Récupère l'identifiant de l'utilisateur actuellement connecté
+        $posts = $repo->findBy(['my_user' => $user_id]); // Récupère tous les posts associés à l'utilisateur connecté
+
+        return $this->render('user/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/settings', name: 'settings')]
+    public function settings(Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/profile_pictures')] string $profilePicturesDirectory, #[Autowire('%kernel.project_dir%/public/uploads/banners')] string $bannersDirectory): Response
+    {
+        // Je récupère les informations de l'utilisateur connecté
+        $userForm = $this->getUser();
+
+        // Initialisation du formulaire
+        $editUserForm = $this->createForm(UserSettingsType::class, $userForm);
+
+        // Traitement du formulaire
+        $editUserForm->handleRequest($request);
+
+        // Si le formulaire est envoyé (isSubmitted) et que ces données sont valides (isValid())
+        if ($editUserForm->isSubmitted() && $editUserForm->isValid()) {
+
+            // Récupère la valeur de l'input "profile_picture_name" et la stocke dans la variable $profilePicture
+            $profilePicture = $editUserForm->get('profilePictureName')->getData();
+            $banner = $editUserForm->get('bannerName')->getData();
+
+            // Je récupère toutes les données du formulaire (rempli) et les injectes dans l'objet $post
+            $completePost = $editUserForm->getData();
+
+            // Si la valeur du champ de saisie "image_name" (stockée dans la variable $profilePicture) n'est pas vide...
+            if ($profilePicture) {
+                // Je récupère le nom original de l'image
+                $originalImageName = pathinfo($profilePicture->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // Puis j'utilise un slug pour créer un nom sécurisé
+                $safeFilename = $slugger->slug($originalImageName);
+
+                /* Enfin, je crée le nom définitif de l'image en utilisant la valeur de la variable $safeFileName, j'inclus
+                un identifiant unique grâce à la fonction "uniqid()", puis je précise l'extension de l'image grâce à la fonction
+                "guessExtension()" appliquée sur la variable $image.
+                */
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $profilePicture->guessExtension();
+
+                try {
+                    // Envoie l'image dans le dossier adéquat
+                    $profilePicture->move($profilePicturesDirectory, $newFilename);
+                } catch (FileException $e) {
+                    $message = $e;
+                }
+
+                // Stocke le nom de l'image dans la BDD
+                $editUserForm->setProfilePictureName($newFilename);
+            }
+
+            if ($banner) {
+                // Je récupère le nom original de l'image
+                $originalImageName = pathinfo($banner->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // Puis j'utilise un slug pour créer un nom sécurisé
+                $safeFilename = $slugger->slug($originalImageName);
+
+                /* Enfin, je crée le nom définitif de l'image en utilisant la valeur de la variable $safeFileName, j'inclus
+                un identifiant unique grâce à la fonction "uniqid()", puis je précise l'extension de l'image grâce à la fonction
+                "guessExtension()" appliquée sur la variable $image.
+                */
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $banner->guessExtension();
+
+                try {
+                    // Envoie l'image dans le dossier adéquat
+                    $banner->move($bannersDirectory, $newFilename);
+                } catch (FileException $e) {
+                    $message = $e;
+                }
+
+                // Stocke le nom de l'image dans la BDD
+                $editUserForm->setBannerName($newFilename);
+            }
+
+            // J'ajoute la date de création (setter) au champ createdAt du formulaire (caché)
+            $completePost->setCreatedAt(new \DateTimeImmutable());
+
+            $user = $this->getUser(); // Je récupère les données l'utilisateur connecté
+            $completePost->setMyUser($user); // Puis j'associe les données de l'utilisateur connecté à la publication (setter)
+
+            $em->persist($completePost); // Prépare la requête (ici, la création d'un nouveau post)
+            $em->flush(); // Exécute la requête préparée
+        }
+
+        // Refactor cette méthode pour récupérer les informations de l'utilisateur connecté
+        return $this->render('user/userSettings/settings.html.twig', [
+            'editUserForm' => $editUserForm
+        ]);
+    }
+}
