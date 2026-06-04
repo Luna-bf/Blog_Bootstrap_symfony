a5fd47d3a62100e1775d23dcd1afc6165cf16eac
Luna-bf <lburattifayolle@gmail.com>
2026-06-04 11:29:23 +0200

    Màj du nom du dossier

---
+++ src/Service/BannerUploader.php
@@ -0,0 +1,36 @@
+<?php
+
+// src/Service/BannerUploader.php
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class BannerUploader {
+    public function __construct(
+        private string $bannersDirectory,
+        private SluggerInterface $slugger,
+    ) {
+    }
+
+    public function upload(UploadedFile $file): string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $bannerName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->getTargetDirectory(), $bannerName);
+        } catch (FileException $e) {
+            throw new FileException("Une erreur est survenue.");
+        }
+
+        return $bannerName;
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->bannersDirectory;
+    }
+}
