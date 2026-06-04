29fcb3ff7aa6f163c50bf575b72a1c960fec3185
Luna-bf <lburattifayolle@gmail.com>
2026-06-04 11:45:44 +0200

    - Ajout de la logique d'upload d'image (bannière, photo de profil) - Ajout des routes vers les formulaire de réinitialisation du mot de passe et de l'adresse mail

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,21 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Post;
 use App\Entity\User;
 use App\Form\UserSettingsType;
 use App\Repository\PostRepository;
+use App\Service\BannerUploader;
+use App\Service\ProfilePictureUploader;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 // #[IsGranted('ROLE_USER')] : Ces routes ne sont accessibles que si l'utilisateur est connecté
 #[IsGranted('ROLE_USER')]
@@ -46,16 +43,6 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/user/{id}', name: 'show_profile')]
-    public function showPostProfile(Post $post)
-    {
-        $userPosts = $post->getMyUser();
-
-        return $this->render('post/show/showProfile.html.twig', [
-            'userPosts' => $userPosts
-        ]);
-    }
-
     /*
     La ligne "#[CurrentUser] User $user" va récupérer toutes les informations de l'utilisateur actuellement connecté :
 
@@ -63,23 +50,15 @@ final class UserController extends AbstractController
     - #[CurrentUser] : est un attribut PHP qui va me permettre de récupérer toutes les informations de l'utilisateur connecté
     */
     #[Route('/settings', name: 'settings')]
-    public function settings(#[CurrentUser] User $user): Response
-    {
-
-        $editUserForm = $this->createForm(UserSettingsType::class, $user);
-
-        return $this->render('user/userSettings/settings.html.twig', [
-            'editUserForm' => $editUserForm
-        ]);
-    }
-
-    #[Route('/settings/profilePicture', name: 'profile_picture_settings')]
-    public function profilePictureSettings(#[CurrentUser] User $user, Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/user_images/profile_pictures')] string $profilePicturesDirectory): Response
+    public function settings(#[CurrentUser] User $user, Request $request, ProfilePictureUploader $profilePictureUploader, BannerUploader $bannerUploader): Response
     {
         /* J'ai ajouté un nouveau paramètre dans le fichier services.yaml nommé "user_images_directory", qui va contenir toutes
         les images associées à l'utilisateur */
-        $oldProfilePicture = $this->getParameter("user_images_directory") . '/profile_pictures/' . $user->getProfilePictureName(); // Récupère la photo de profil actuelle
+        $oldProfilePicture = $profilePictureUploader->getTargetDirectory() . '/' . $user->getProfilePictureName(); // Récupère la photo de profil actuelle
         $newProfilePicture = "";
+
+        $oldBanner = $bannerUploader->getTargetDirectory() . '/' . $user->getBannerName(); // Récupère la bannière de profil actuelle
+        $newBanner = "";
 
         // Initialisation du formulaire
         $editUserForm = $this->createForm(UserSettingsType::class, $user);
@@ -92,133 +71,82 @@ final class UserController extends AbstractController
 
             // Récupère la valeur de l'input "profile_picture_name" et la stocke dans la variable $newProfilePicture
             $newProfilePicture = $editUserForm->get('profile_picture_name')->getData();
+            $newBanner = $editUserForm->get('banner_name')->getData();
 
             if ($newProfilePicture) {
 
-                if (is_dir($oldProfilePicture)) {
+                if (is_dir($oldBanner)) {
 
-                    $originalFilename = pathinfo($newProfilePicture->getClientOriginalName(), PATHINFO_FILENAME);
-                    $safeFilename = $slugger->slug($originalFilename);
-                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $newProfilePicture->guessExtension();
+                    // Utilise le service "ProfilePictureUploader" pour traiter l'envoi du fichier
+                    $newFilename = $profilePictureUploader->upload($newProfilePicture);
 
-                    try {
-                        $newProfilePicture->move($profilePicturesDirectory, $newFilename);
-                    } catch (FileException $e) {
-                        $message = $e;
-                    }
-
-                    // Met à jour le nom du fichier dans la BDD
+                    // Stocke le nom du fichier dans la BDD
                     $user->setProfilePictureName($newFilename);
 
-                    // Sinon, si je récupère un fichier :
+                // Sinon, si je récupère un fichier :
                 } else {
                     unlink($oldProfilePicture);
 
-                    // Je récupère le nom original de l'image
-                    $originalImageName = pathinfo($newProfilePicture->getClientOriginalName(), PATHINFO_FILENAME);
+                    // Utilise le service "ProfilePictureUploader" pour traiter l'envoi du fichier
+                    $newFilename = $profilePictureUploader->upload($newProfilePicture);
 
-                    // Puis j'utilise un slug pour créer un nom sécurisé
-                    $safeFilename = $slugger->slug($originalImageName);
-
-                    /* Enfin, je crée le nom définitif de l'image en utilisant la valeur de la variable $safeFileName, j'inclus
-                    un identifiant unique grâce à la fonction "uniqid()", puis je précise l'extension de l'image grâce à la
-                    fonction "guessExtension()" appliquée sur la variable $image. */
-                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $newProfilePicture->guessExtension();
-
-                    try {
-                        // Envoie l'image dans le dossier adéquat
-                        $newProfilePicture->move($profilePicturesDirectory, $newFilename);
-                    } catch (FileException $e) {
-                        $message = $e;
-                    }
-
-                    // Stocke le nom de l'image dans la BDD
+                    // Met à jour le nom du fichier dans la BDD
                     $user->setProfilePictureName($newFilename);
                 }
             }
 
-            $em->persist($user); // Prépare la requête
-            $em->flush(); // Exécute la requête préparée
+            if ($newBanner) {
 
-            return $this->redirectToRoute('user_index');
+                if (is_dir($oldBanner)) {
+
+                    // Utilise le service "BannerUploader" pour traiter l'envoi du fichier
+                    $newFilename = $bannerUploader->upload($newBanner);
+
+                    // Stocke le nom du fichier dans la BDD
+                    $user->setBannerName($newFilename);
+
+                    // Sinon, si je récupère un fichier :
+                } else {
+                    unlink($oldBanner);
+
+                    // Utilise le service "BannerUploader" pour traiter l'envoi du fichier
+                    $newFilename = $bannerUploader->upload($newBanner);
+
+                    // Met à jour le nom du fichier dans la BDD
+                    $user->setBannerName($newFilename);
+                }
+            }
         }
 
-        return $this->render('user/userSettings/pictures/profilePictureSettings.html.twig', [
+        return $this->render('user/userSettings/settings.html.twig', [
             'editUserForm' => $editUserForm
         ]);
     }
 
-    #[Route('/settings/banner', name: 'banner_settings')]
-    public function bannerSettings(#[CurrentUser] User $user, Request $request, EntityManagerInterface $em, SluggerInterface $slugger, #[Autowire('%kernel.project_dir%/public/uploads/user_images/banners')] string $bannersDirectory): Response
+    #[Route('/settings/emailReset', name: 'email_reset')]
+    public function emailReset(#[CurrentUser] User $user, Request $request, EntityManagerInterface $em): Response
     {
-
-        /* J'ai ajouté un nouveau paramètre dans le fichier services.yaml nommé "user_images_directory", qui va contenir toutes
-        les images associées à l'utilisateur */
-        $oldBanner = $this->getParameter("user_images_directory") . '/banners/' . $user->getBannerName(); // Récupère la photo de profil actuelle
-        $newBanner = "";
-
         // Initialisation du formulaire
         $editUserForm = $this->createForm(UserSettingsType::class, $user);
 
         // Traitement du formulaire
         $editUserForm->handleRequest($request);
 
-        // Si le formulaire est envoyé (isSubmitted) et que ses données sont valides (isValid())
-        if ($editUserForm->isSubmitted() && $editUserForm->isValid()) {
+        return $this->render('user/userSettings/emailReset.html.twig', [
+            'editUserForm' => $editUserForm
+        ]);
+    }
 
-            // Récupère la valeur de l'input "banner_name" et la stocke dans la variable $newBanner
-            $newBanner = $editUserForm->get('banner_name')->getData();
+    #[Route('/settings/passwordReset', name: 'password_reset')]
+    public function passwordReset(#[CurrentUser] User $user, Request $request, EntityManagerInterface $em): Response
+    {
+        // Initialisation du formulaire
+        $editUserForm = $this->createForm(UserSettingsType::class, $user);
 
-            if ($newBanner) {
+        // Traitement du formulaire
+        $editUserForm->handleRequest($request);
 
-                if (is_dir($oldBanner)) {
-                    
-                    $originalFilename = pathinfo($newBanner->getClientOriginalName(), PATHINFO_FILENAME);
-                    $safeFilename = $slugger->slug($originalFilename);
-                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $newBanner->guessExtension();
-
-                    try {
-                        $newBanner->move($bannersDirectory, $newFilename);
-                    } catch (FileException $e) {
-                        $message = $e;
-                    }
-
-                    // Met à jour le nom du fichier dans la BDD
-                    $user->setBannerName($newFilename);
-
-                } else {
-                    unlink($oldBanner);
-
-                    // Je récupère le nom original de l'image
-                    $originalImageName = pathinfo($newBanner->getClientOriginalName(), PATHINFO_FILENAME);
-
-                    // Puis j'utilise un slug pour créer un nom sécurisé
-                    $safeFilename = $slugger->slug($originalImageName);
-
-                    /* Enfin, je crée le nom définitif de l'image en utilisant la valeur de la variable $safeFileName, j'inclus
-                    un identifiant unique grâce à la fonction "uniqid()", puis je précise l'extension de l'image grâce à la
-                    fonction "guessExtension()" appliquée sur la variable $image. */
-                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $newBanner->guessExtension();
-
-                    try {
-                        // Envoie l'image dans le dossier adéquat
-                        $newBanner->move($bannersDirectory, $newFilename);
-                    } catch (FileException $e) {
-                        $message = $e;
-                    }
-
-                    // Stocke le nom de l'image dans la BDD
-                    $user->setBannerName($newFilename);
-                }
-            }
-
-            $em->persist($user); // Prépare la requête
-            $em->flush(); // Exécute la requête préparée
-
-            return $this->redirectToRoute('user_index');
-        }
-
-        return $this->render('user/userSettings/pictures/bannerSettings.html.twig', [
+        return $this->render('user/userSettings/passwordReset.html.twig', [
             'editUserForm' => $editUserForm
         ]);
     }
